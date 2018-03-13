56d44c887256559c034c76a275ef44a92bd75bec
cuongqle <lananh30011985>
2018-03-13 10:08:12 +0700

    first commit for template

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('ccfs App', () => {
+describe('CCFS App', function() {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
