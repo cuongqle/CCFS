66eeedf44dc146530926fe82043f8aea2efc57ba
cuongqle <lananh30011985>
2018-03-20 18:48:16 +0700

    navigator

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -4,40 +4,38 @@ export const navigation = [
     url: '/dashboard',
     badge: {
       variant: 'info'
-    },
-    children: [
-      {
-        name: 'Debtor',
-        url: '/debtor',
-        icon: 'fa fa-money',
-        badge: {
-          variant: 'info'
-        }
-      },
-      {
-        name: 'CC Letter',
-        url: '/cc-letter',
-        icon: 'icon-envelope-letter',
-        badge: {
-          variant: 'info'
-        }
-      },
-      {
-        name: 'Templates',
-        url: '/templates',
-        icon: 'icon-envelope-letter',
-        badge: {
-          variant: 'info'
-        }
-      },
-      {
-        name: 'CC Letters',
-        url: '/cc-letters',
-        icon: 'icon-envelope-letter',
-        badge: {
-          variant: 'info'
-        }
-      }
-    ]
+    }
+  },
+  {
+    name: 'Debtor',
+    url: '/debtor',
+    icon: 'fa fa-money',
+    badge: {
+      variant: 'info'
+    }
+  },
+  {
+    name: 'CC Letter',
+    url: '/cc-letter',
+    icon: 'icon-envelope-letter',
+    badge: {
+      variant: 'info'
+    }
+  },
+  {
+    name: 'Templates',
+    url: '/templates',
+    icon: 'icon-envelope-letter',
+    badge: {
+      variant: 'info'
+    }
+  },
+  {
+    name: 'CC Letters',
+    url: '/cc-letters',
+    icon: 'icon-envelope-letter',
+    badge: {
+      variant: 'info'
+    }
   }
 ];
