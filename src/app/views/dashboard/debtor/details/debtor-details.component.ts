e8d504b2fa6a3dcdd0b8f515bbebac1afa7d1c5b
cuongqle <lananh30011985>
2018-03-29 10:11:15 +0700

    debtor details

--- src/app/views/dashboard/debtor/details/debtor-details.component.ts
+++ src/app/views/dashboard/debtor/details/debtor-details.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Debtor} from "../../../../dtos/debtorDTOs";
 
 @Component({
   templateUrl: 'debtor-details.component.html'
 })
 export class DebtorDetailsComponent {
+  debtor: Debtor;
+
   constructor(private activatedRouter: ActivatedRoute) {
     this.debtor = this.activatedRouter.snapshot.data['resolverGetDebtorById'];
   }
