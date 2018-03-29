8b9c161331f44611adc82703727fe99ba0167550
cuongqle <lananh30011985>
2018-03-29 17:17:24 +0700

    debtor detail trigger save, add form

--- src/app/views/dashboard/debtor/details/debtor-details.component.ts
+++ src/app/views/dashboard/debtor/details/debtor-details.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Debtor} from "../../../../dtos/debtorDTOs";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   templateUrl: 'debtor-details.component.html'
 })
-export class DebtorDetailsComponent {
+export class DebtorDetailsComponent implements OnInit {
   debtor: Debtor;
 
-  constructor(private activatedRouter: ActivatedRoute) {
+  constructor(private activatedRouter: ActivatedRoute, private formBuilder: FormBuilder) {
     this.debtor = this.activatedRouter.snapshot.data['resolverGetDebtorById'];
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+    });
+  }
+
+  save() {
+
   }
 }
